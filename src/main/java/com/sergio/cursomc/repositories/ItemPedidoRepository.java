c274affff34844a557e79835df3650c2edf6c933
Sergio Junior <sergio.ltnj@gmail.com>
2019-07-14 20:51:52 -0300

    Item pedido e item pedido pk

---
+++ src/main/java/com/sergio/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.sergio.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sergio.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+}
