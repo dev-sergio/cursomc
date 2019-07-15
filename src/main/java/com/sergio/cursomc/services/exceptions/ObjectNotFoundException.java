26b7da68447a8a3e4e671fd4969d2481808fbda4
Sergio Junior <sergio.ltnj@gmail.com>
2019-07-15 20:07:39 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/sergio/cursomc/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/sergio/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -1,6 +1,7 @@
 package com.sergio.cursomc.services.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException {
+	
 	private static final long serialVersionUID = 1L;
 
 	public ObjectNotFoundException(String msg) {
