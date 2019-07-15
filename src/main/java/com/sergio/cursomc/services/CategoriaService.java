a87c469ba2439934c53a65a7dce1a6ba70565dc9
Sergio Junior <sergio.ltnj@gmail.com>
2019-07-15 19:46:21 -0300

    Validacao Sistatica com bean validation

--- src/main/java/com/sergio/cursomc/services/CategoriaService.java
+++ src/main/java/com/sergio/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.sergio.cursomc.DTO.CategoriaDTO;
 import com.sergio.cursomc.domain.Categoria;
 import com.sergio.cursomc.repositories.CategoriaRepository;
 import com.sergio.cursomc.services.exceptions.DataIntegrityException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
